33b3932dace60cb507394b494759b09ac7830499
Brian Crisp <bcrisp084@yahoo.com>
2021-05-02 19:31:14 -0400

    minor style change

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header () {
     return (
         <div className="header">
             <h1>Employee Directory</h1>
-            <p>Click on each heading to filter or use the search box to narrow your results</p>
+            <p>Click on the name heading or type in the search box to narrow your results</p>
         </div>
     )
 }
