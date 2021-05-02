01e9c8ac75dee923dd7ef0295ed3bc92fedfbba0
Brian Crisp <bcrisp084@yahoo.com>
2021-05-02 16:58:00 -0400

    added data for page load inside components

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+  
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>My Employee Directory</h1>
+            <p>Click on the search box to narrow your results</p>
+        </div>
+    )
+}
+
+export default Header;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,12 +1,13 @@
-import axios from "axios";
-import REACT from "react";
+import React from "react";
 import DataArea from "../DataArea/index.js";
 import "./Main.css";
 
-function Main () {
+function Main (){
     return (
-        <div>
-            <DataArea />
-        </div>
+        <>
+        <DataArea />
+        </>
     )
-}
+};
+
+export default Main;
